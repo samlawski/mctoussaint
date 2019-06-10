1148b1e025b6fcca5ba4ae52bcc4772c149583aa
Sam <sam.bogu@gmail.com>
2019-06-10 13:20:43 +0200

    added cookie banner

--- _custom-theme/assets/main.js
+++ _custom-theme/assets/main.js
@@ -69,58 +69,67 @@ $('#booking').click(function(){
 // })
 
 // Cookie Banner
-// var cookieBanner = (function(){
-//   var init = function(){
-//     showCookieBanner();
-//   };
-//
-//   function readCookie(n) {
-//     let a = `; ${document.cookie}`.match(`;\\s*${n}=([^;]+)`);
-//     return a ? a[1] : '';
-//   }
-//
-//   function showCookieBanner(){
-//     if(!readCookie('cookieBannerDismissed')){
-//       var banner_wrapper = document.createElement('div');
-//       banner_wrapper.setAttribute("id", "cookieBanner");
-//       banner_wrapper.style.cssText = `
-//         font-size: 0.7rem;
-//         background-color: #ddd;
-//         color: #444;
-//         max-width: 240px;
-//         padding: 16px;
-//         position: fixed;
-//         bottom: 0;
-//         left: 0;`;
-//
-//       var banner_text = document.createElement('p');
-//       banner_text.innerHTML = `
-//         Um unsere Webseite für Sie optimal zu gestalten und fortlaufend verbessern zu können, verwenden wir Cookies und Analysetools. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung von Cookies zu. Weitere Informationen zur Nutzung von Cookies und Analysetools erhalten Sie in unserer <a href="/datenschutz">Datenschutzerklärung</a>.
-//         `;
-//       banner_wrapper.appendChild(banner_text);
-//
-//       var banner_link = document.createElement('a');
-//       banner_link.innerHTML = `Verstanden`;
-//       banner_link.style.cssText = `
-//         margin-top: 8px;
-//         padding: 4px;
-//         border: 1px solid #444;
-//         display: inline-block;
-//         cursor: pointer;`;
-//       banner_link.addEventListener('click', function(){
-//         document.cookie = "cookieBannerDismissed=true";
-//         banner_wrapper.style.display = "none";
-//       })
-//       banner_wrapper.appendChild(banner_link);
-//
-//       document.body.appendChild(banner_wrapper);
-//     }
-//   }
-//
-//
-//   return {
-//     init: init
-//   }
-// })()
-// TODO: Activate if Google Analytics will be set up
-// cookieBanner.init()
+var cookieBanner = (function(){
+  var init = function(){
+    showCookieBanner();
+  };
+
+  function readCookie(n) {
+    let a = `; ${document.cookie}`.match(`;\\s*${n}=([^;]+)`);
+    return a ? a[1] : '';
+  }
+
+  function showCookieBanner(){
+    if(!readCookie('cookieBannerDismissed')){
+      var banner_wrapper = document.createElement('div');
+      banner_wrapper.setAttribute("id", "cookieBanner");
+      banner_wrapper.style.cssText = `
+        font-size: 0.6rem;
+        background-color: #ddd;
+        color: #444;
+        width: 100%;
+        text-align: center;
+        padding: 10px;
+        position: fixed;
+        bottom: 0;
+        left: 0;`;
+
+      var banner_spacer = document.createElement('div');
+      banner_spacer.setAttribute("id", "cookieBannerSpacer");
+      banner_spacer.style.cssText = `
+        height: 106px;
+      `;
+
+      var banner_text = document.createElement('p');
+      banner_text.innerHTML = `
+        In order to optimize our website for you we use cookies and analysis tools. By continuing to use the website, you consent to the use of cookies. For more information, please see our <a href="/privacy">Privacy Policy</a>.
+        `;
+      banner_wrapper.appendChild(banner_text);
+
+      var banner_link = document.createElement('a');
+      banner_link.innerHTML = `Ok`;
+      banner_link.style.cssText = `
+        margin-top: 6px;
+        padding: 4px;
+        border: 1px solid #444;
+        display: inline-block;
+        cursor: pointer;`;
+      banner_link.addEventListener('click', function(){
+        document.cookie = "cookieBannerDismissed=true";
+        banner_wrapper.style.display = "none";
+        banner_spacer.style.display = "none";
+      })
+      banner_wrapper.appendChild(banner_link);
+
+      document.body.appendChild(banner_wrapper);
+      document.body.appendChild(banner_spacer);
+    }
+  }
+
+
+  return {
+    init: init
+  }
+})()
+
+cookieBanner.init()
